f42073d88371410a4851ddf61da7823efa927748
SaiyemBejoy <jsbejoy96@gmail.com>
2020-05-28 22:22:44 +0600

    Getting started

---
+++ src/main/java/com/example/web/AlumniAppWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniAppWsApplication.class, args);
+	}
+
+}
